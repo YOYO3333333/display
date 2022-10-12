e77c1eff4b9f990c7eeb7da112252d642c6189c9
YOYO3333333 <114952052+YOYO3333333@users.noreply.github.com>
2022-10-12 16:17:58 +0200

    Update display.c

--- display.c
+++ display.c
@@ -3,11 +3,13 @@ void display_square(int width)
 {
     int iter = 0;
 
-    for (int i = 0; i < width-1 ; i++)
+
+
+    for (int i = 0; i < width; i++)
     {
         putchar('*');
     }
-    putchar('*');
+    putchar('\n');
     if (!(width % 2))
     {
         iter = ((width + 1) / 2) - 1;
@@ -16,35 +18,35 @@ void display_square(int width)
     {
         iter = ((width + 1) / 2) - 2;
     }
-    for (int i = 0; i < iter; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            if (!j)
+	for (int i=0 ;i <iter ;i++)
+	{
+       for (int j=0; j < width;j++)
+		{
+			if (!j)
             {
                 putchar('*');
-                continue;
-            }
-            else if (j == width - 1)
-            {
+				int k=0;
+				while(k < width-2)
+				{  
+				 putchar(' ');
+				 k++;
+				}
                 putchar('*');
+				putchar('\n');
                 continue;
             }
-            putchar(' ');
-        }
-    }
-    for (int i = 0; i < width - 1; i++)
+		}
+	}
+
+    for (int i = 0; i < width; i++)
     {
         putchar('*');
-
     }
-    putchar('*');
+	putchar('\n');
 
 }
 
-
 int main(void)
 {
-display_square(11);
-
+display_square(4);
 }
