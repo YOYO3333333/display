6242541a0e9fb6659b8715eab5cc41b846d556c9
YOYO3333333 <114952052+YOYO3333333@users.noreply.github.com>
2022-11-23 17:45:12 +0100

    Update display.c

--- display.c
+++ display.c
@@ -1,52 +1,47 @@
+
+#include <stddef.h>
 #include <stdio.h>
 void display_square(int width)
 {
-    int iter = 0;
-
-
-
+    if (width <= 0)
+        return;
+    if (width % 2 == 0)
+        width++;
+    if (width == 1)
+    {
+        putchar(42);
+        putchar(10);
+        return;
+    }
     for (int i = 0; i < width; i++)
     {
-        putchar('*');
+        putchar(42);
     }
-    putchar('\n');
-    if (!(width % 2))
+    putchar(10);
+    for (int i = 1; i < width / 2; i++)
     {
-        iter = ((width + 1) / 2) - 1;
+tr       putchar(42);
+        for (int j = 0; j < width - 2; j++)
+        {
+            putchar(32);
+        }
+        putchar(42);
+        putchar(10);
     }
-    else
-    {
-        iter = ((width + 1) / 2) - 2;
-    }
-	for (int i=0 ;i <iter ;i++)
-	{
-       for (int j=0; j < width;j++)
-		{
-			if (!j)
-            {
-                putchar('*');
-				int k=0;
-				while(k < width-2)
-				{  
-				 putchar(' ');
-				 k++;
-				}
-                putchar('*');
-				putchar('\n');
-                continue;
-            }
-		}
-	}
-
     for (int i = 0; i < width; i++)
     {
-        putchar('*');
+        putchar(42);
     }
-	putchar('\n');
-
+    putchar(10);
 }
 
-int main(void)
+
+int main()
 {
-display_square(4);
+    struct stack *s = NULL;
+    s = stack_push(s, 5);
+    printf("%d\n", s->data);
+    stack_pop(s);
+    printf("%p\n", (void*)s);
+    return 0;
 }
